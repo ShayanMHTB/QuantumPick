208ead62a0bc292f38313e5bb180965fff0d76f0
Shayan Mahtabi <shayan.mhtb@gmail.com>
2025-05-15 01:34:59 +0200

    Mock USDT stablecoin solidity file generated

---
+++ ctr/contracts/MockUSDT.sol
@@ -0,0 +1,15 @@
+// contracts/MockUSDT.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockUSDT is ERC20 {
+    constructor() ERC20("Tether USD", "USDT") {
+        _mint(msg.sender, 10000000 * 10 ** decimals()); // Mint 10,000,000 USDT
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 6; // USDT uses 6 decimals
+    }
+}
